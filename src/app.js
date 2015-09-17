e1497e29b05330cd6ea7cb9bcaafbf8b642a0d91
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-09-17 21:49:47 +1000

    fix(route): remove push state or gh-pages does not work

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 export class App {
   configureRouter(config, router){
     config.title = 'Aurelia JSPM Registry';
-    config.options.pushState = true;
+
     config.map([
       { route: [''], name: 'home', moduleId: 'home', nav: true, title:'Home' }
     ]);
